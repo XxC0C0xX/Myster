a07a2895b406b5e9e9e1ff14195b4967db81e67e
XxC0C0xX <152925524+XxC0C0xX@users.noreply.github.com>
2023-12-06 00:29:28 +0100

    Update ddosPC.py

--- ddosPC.py
+++ ddosPC.py
@@ -103,24 +103,29 @@ def ddos():
 	# Vérifie que l'adresse IP est fonctionnel
 	style()
 	ipSend = inputYellow("\n[1] IP or WebSite Adress : ")
-	if ipSend[0:8] == "https://":
-		ipSend = ipSend[8:len(ipSend)]
+	if ipSend != "":
+		if ipSend[0:8] == "https://":
+			ipSend = ipSend[8:len(ipSend)]
 	
-	elif ipSend[0:7] == "http://":
-		ipSend = ipSend[7:len(ipSend)]
+	        elif ipSend[0:7] == "http://":
+		        ipSend = ipSend[7:len(ipSend)]
 	
-	for i in range(len(ipSend)):
-		if ipSend[i]=="/":
-			ipSend = ipSend[0:i]
-			break
-	while True:
-		try:
-			ip = socket.gethostbyname(ipSend)
-			break	
-		except:
-			style()
-			printRed("\n                      Error : IP Adress is Not Found")
-			ipSend = inputYellow("\n[1] IP or WebSite Adress : ")
+	        for i in range(len(ipSend)):
+		        if ipSend[i]=="/":
+			        ipSend = ipSend[0:i]
+			        break
+	        while True:
+		        try:
+			        ip = socket.gethostbyname(ipSend)
+			        break	
+		        except:
+				style()
+				printRed("\n                      Error : IP Adress is Not Found")
+				ipSend = inputYellow("\n[1] IP or WebSite Adress : ")
+	else:
+		style()
+		printRed("\n                      Error : IP Adress is Not Found")
+		ipSend = inputYellow("\n[1] IP or WebSite Adress : ")
 
 	# Vérifie que bytes est dans [1 ;65507]
 	style()		
